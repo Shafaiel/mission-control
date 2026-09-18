467f748c8ce47911979bb57f78460536af62e5ad
Shafaiel <shafaiel5@gmail.com>
2026-09-18 21:02:15 +0500

    Add Header component with title and subtitle props

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header({ title, subtitle }) {
+  return (
+    <header className="header">
+      <h1 className="header__title">{title}</h1>
+      {subtitle && <p className="header__subtitle">{subtitle}</p>}
+    </header>
+  )
+}
+
+export default Header
